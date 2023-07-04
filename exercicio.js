dd46a73439e8ffd9bc8ad20815390ec886a8fc41
Gabriela Teixeira <gabriela.teixeira92@gmail.com>
2023-07-03 22:09:42 -0300

    exercicio ts

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+function calculaMultiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+const calculaMultiplicacao2 = (numero1, numero2) => numero1 * numero2;
+const resutadoMultiplicacao1 = calculaMultiplicacao(5, 2);
+console.log(resutadoMultiplicacao1);
---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+function calculaMultiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+const calculaMultiplicacao2 = (numero1: number, numero2: number): number => numero1 * numero2;
+
+
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+const saudacao2 = (nome: string): string => `Olá ${nome}`;
